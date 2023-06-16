ea448af4b9df85c5e06c290aed3c5a85c283c000
Pratish Ninawe <pratish.1006@gmail.com>
2023-06-16 22:03:19 +0530

    final commit

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 app.use("/users", userRoutes);
